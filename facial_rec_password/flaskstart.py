8bc9452c8d093b68ecffe4d9e93b7f646ea4fa4c
Tarik Amraoui <ta355@njit.edu>
2024-02-12 21:57:22 -0500

    finally, live video feed and encoding works

--- facial_rec_password/flaskstart.py
+++ facial_rec_password/flaskstart.py
@@ -12,45 +12,51 @@ camera = cv2.VideoCapture(0)
 # Global variable to store the latest frame from the camera
 latest_frame = None
 
-def capture_frames():
-    global latest_frame
-    while True:
-        ret, frame = camera.read()
-        if ret:
-            latest_frame = frame
-
+            
 def generate_video_feed():
     global latest_frame
     while True:
-        ret, frame = camera.read()  # Read frame from the global camera object
+        ret, frame = camera.read()  
         if ret:
-            latest_frame = frame  # Update the latest frame
 
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                continue
+            
+            frame_bytes = buffer.tobytes()
+            
             # Detect face locations in the frame
             face_locations = face_recognition.face_locations(frame)
 
             if face_locations:
                 # Convert frame to RGB format for face_recognition
                 rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                # Encode the face(s) in the frame
                 face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
                 encodings_list = [encoding.tolist() for encoding in face_encodings]
                 encodings_json = json.dumps(encodings_list)
-                yield encodings_json  # Yield the face encodings as JSON
+
+                # Yield the face encodings along with the frame
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n' + encodings_json.encode() + b'\r\n')
+            
             else:
-                yield json.dumps([])  # Yield an empty list if no faces are detected
+                # Yield only the frame if no faces are detected
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
         else:
-            continue 
+            continue
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/')
+@app.route('/video_feed')
 def video_feed():
     return Response(generate_video_feed(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    capture_thread = threading.Thread(target=capture_frames)
+    #set up different thread for genererate_video_feed 
+    capture_thread = threading.Thread(target=generate_video_feed)
     capture_thread.daemon = True
     capture_thread.start()
     
